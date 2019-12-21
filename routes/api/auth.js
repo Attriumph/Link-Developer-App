d6bcb6eb0f20d7c52469eb90319c45d2ec6c05a8
lingquan <lingquanhan@gmail.com>
2019-12-20 17:35:44 -0800

    Add: JWT Authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @router  GET api/auth
 // @desc    Test router
 // @access  Public
-router.get('/', (req, res) => res.send('Auth router'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
