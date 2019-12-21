10a361f199828a97a2594c33ef48359c38cfcdde
lingquan <lingquanhan@gmail.com>
2019-12-20 22:01:36 -0800

    Add: get my profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth');
 
-// @router  GET api/profile
-// @desc    Test router
-// @access  Public
-router.get('/', (req, res) => res.send('Profile router'));
+// @router  GET api/profile/me
+// @desc    Get the profile of current user
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'No profile for this user.' });
+    }
+
+    res.send(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
