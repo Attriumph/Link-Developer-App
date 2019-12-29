497a0a9c97e9e27a9c66c387c721b184b0f23e80
lingquan <lingquanhan@gmail.com>
2019-12-28 21:48:28 -0800

    Add: get current profile

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
-export default combineReducers({ alert, auth });
+import profile from './profile';
+
+export default combineReducers({ alert, auth, profile });
