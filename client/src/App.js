cf89e840fb19d07985bed81a4033e6beed4cb560
lingquan <lingquanhan@gmail.com>
2019-12-31 23:33:02 -0800

    Add: Add experience and edication

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 
@@ -38,8 +41,23 @@ const App = () => {
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </section>
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState, Fragment } from 'react';
-// in order to use history to redirect in action, we need withRouter
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createProfile } from '../../actions/profile';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
-const CreateProfile = ({ createProfile, history }) => {
+const EditProfile = ({
+  profile: { profile, loading },
+  createProfile,
+  getCurrentProfile,
+  history
+}) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -20,7 +24,29 @@ const CreateProfile = ({ createProfile, history }) => {
     youtube: '',
     instagram: ''
   });
+
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  useEffect(() => {
+    getCurrentProfile();
+
+    setFormData({
+      company: loading || !profile.company ? '' : profile.company,
+      website: loading || !profile.website ? '' : profile.website,
+      location: loading || !profile.location ? '' : profile.location,
+      status: loading || !profile.status ? '' : profile.status,
+      skills: loading || !profile.skills ? '' : profile.skills.join(','),
+      githubusername:
+        loading || !profile.githubusername ? '' : profile.githubusername,
+      bio: loading || !profile.bio ? '' : profile.bio,
+      twitter: loading || !profile.social ? '' : profile.social.twitter,
+      facebook: loading || !profile.social ? '' : profile.social.facebook,
+      linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+      youtube: loading || !profile.social ? '' : profile.social.youtube,
+      instagram: loading || !profile.social ? '' : profile.social.instagram
+    });
+  }, [loading, getCurrentProfile]);
+
   const {
     company,
     website,
@@ -35,24 +61,26 @@ const CreateProfile = ({ createProfile, history }) => {
     youtube,
     instagram
   } = formData;
+
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
   const onSubmit = e => {
     e.preventDefault();
-    createProfile(formData, history);
+    createProfile(formData, history, true);
   };
+
   return (
     <Fragment>
-      <h1 className="large text-primary">Create Your Profile</h1>
+      <h1 className="large text-primary">Edit Your Profile</h1>
       <p className="lead">
-        <i className="fas fa-user" /> Let's get some information to make your
-        profile stand out
+        <i className="fas fa-user" /> Add some changes to your profile
       </p>
       <small>* = required field</small>
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
-            <option value="0">* Select Professional Status</option>
+            <option>* Select Professional Status</option>
             <option value="Developer">Developer</option>
             <option value="Junior Developer">Junior Developer</option>
             <option value="Senior Developer">Senior Developer</option>
@@ -136,6 +164,7 @@ const CreateProfile = ({ createProfile, history }) => {
           />
           <small className="form-text">Tell us a little about yourself</small>
         </div>
+
         <div className="my-2">
           <button
             onClick={() => toggleSocialInputs(!displaySocialInputs)}
@@ -146,6 +175,7 @@ const CreateProfile = ({ createProfile, history }) => {
           </button>
           <span>Optional</span>
         </div>
+
         {displaySocialInputs && (
           <Fragment>
             <div className="form-group social-input">
@@ -158,6 +188,7 @@ const CreateProfile = ({ createProfile, history }) => {
                 onChange={e => onChange(e)}
               />
             </div>
+
             <div className="form-group social-input">
               <i className="fab fa-facebook fa-2x" />
               <input
@@ -168,6 +199,7 @@ const CreateProfile = ({ createProfile, history }) => {
                 onChange={e => onChange(e)}
               />
             </div>
+
             <div className="form-group social-input">
               <i className="fab fa-youtube fa-2x" />
               <input
@@ -178,6 +210,7 @@ const CreateProfile = ({ createProfile, history }) => {
                 onChange={e => onChange(e)}
               />
             </div>
+
             <div className="form-group social-input">
               <i className="fab fa-linkedin fa-2x" />
               <input
@@ -188,6 +221,7 @@ const CreateProfile = ({ createProfile, history }) => {
                 onChange={e => onChange(e)}
               />
             </div>
+
             <div className="form-group social-input">
               <i className="fab fa-instagram fa-2x" />
               <input
@@ -200,6 +234,7 @@ const CreateProfile = ({ createProfile, history }) => {
             </div>
           </Fragment>
         )}
+
         <input type="submit" className="btn btn-primary my-1" />
         <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
@@ -208,8 +243,17 @@ const CreateProfile = ({ createProfile, history }) => {
     </Fragment>
   );
 };
-CreateProfile.propTypes = {
-  createProfile: PropTypes.func.isRequired
+
+EditProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-export default connect(null, { createProfile })(withRouter(CreateProfile));
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
+  withRouter(EditProfile)
+);
