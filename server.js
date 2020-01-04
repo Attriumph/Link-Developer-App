3d8d50af6460c40d94ce1c6ad688c21fe68f4ff2
lingquan <lingquanhan@gmail.com>
2020-01-03 20:30:01 -0800

    prepare for deployment

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const app = express();
 const path = require('path');
 
-// Connect database
+const app = express();
+
+// Connect Database
 connectDB();
 
-// Init Middleware(Bodypaser) for parse the post info
+// Init Middleware
 app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
