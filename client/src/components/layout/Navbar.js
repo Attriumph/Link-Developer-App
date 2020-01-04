0963fd1dc01640097c476e6b8328f302571a44b4
lingquan <lingquanhan@gmail.com>
2020-01-03 19:33:13 -0800

    update

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          <i className="fas fa-code"></i> LinkDevopers
         </Link>
       </h1>
       {!loading && (
